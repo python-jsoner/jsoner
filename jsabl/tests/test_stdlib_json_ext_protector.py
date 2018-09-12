1818482ded224dba8f257cce7ff933c2d513302b
Sylvain Marie <sylvain.marie@schneider-electric.com>
2018-09-12 16:44:36 +0200

    Fixed the unit test about primitive subclass encoding: now refering to the error of stdlib whatever it is (the exact error depends on python version)

--- jsabl/tests/test_stdlib_json_ext_protector.py
+++ jsabl/tests/test_stdlib_json_ext_protector.py
@@ -14,10 +14,12 @@ def test_protection():
     # create an instance
     i = MyInt(12)
 
+    # ------
     # 1- default json lib behaviour: the int is encoded :(
     std_lib_encoder = JSONEncoder()
     assert std_lib_encoder.encode(i) == '12'
 
+    # ------
     # 2- an encoder for which we add the protection, but not the custom object hook
     dummy_encoder = JSONEncoder()
     add_protection_to_json_encoder(dummy_encoder)
@@ -27,8 +29,17 @@ def test_protection():
         dummy_encoder.encode(i)
 
     e = exc_info.value
-    assert str(e) == "Object of type 'MyInt' is not JSON serializable"
+    try:
+        # raise the default exception ...
+        JSONEncoder.default(None, i)
+    except TypeError as e2:
+        # ... so that we can compare
+        assert str(e) == str(e2)
+    else:
+        # we should never reach this line, make sure of that.
+        assert False
 
+    # ------
     # 3- an encoder for which we add the protection AND the custom object hook should encode correctly
     def custom_object_hook(o):
         if isinstance(o, MyInt):
